775e2448c2aac5a74d21c93927ec80345f211a70
IagoGOOl <iago.gool@gmail.com>
2022-09-30 21:51:57 -0300

    adicionar atividade primeiro commit

---
+++ Futebol.java
@@ -0,0 +1,7 @@
+public class Time {
+
+    public static void main(String[] args) {
+        System.out.println("Iago Alves");
+        System.out.println("Corinthians");
+    }
+}
---
+++ futebol (2).java
@@ -0,0 +1,7 @@
+public class time {
+
+    public static void main(string[] args) {
+        system.out.println("iago alves");
+        system.out.println("Corinthians");
+    }
+}
